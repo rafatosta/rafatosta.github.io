aa90ee845b45c8b733d5812cbf18c47b98fb2729
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-01 11:06:04 -0300

    Install Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='w-screen h-screen flex flex-col justify-center items-center gap-6'>
+      <div className='flex flex-row gap-6'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +13,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Rafael Tosta
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1 className="text-3xl font-bold underline">
+        Vite + React + Tailwind CSS
+      </h1>
+    </div>
   )
 }
 
