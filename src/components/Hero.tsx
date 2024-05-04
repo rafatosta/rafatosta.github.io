7aa878707dffa0eaae6bc223ddda75a0f4b21f70
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-04 11:35:05 -0300

    Update hero banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,10 @@ import SocialLinks from "./SocialLinks";
 
 function Hero() {
     return (
-        <div className="relative bg-[url('assets/hero_background.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="relative bg-[url('assets/photo.avif')] bg-cover bg-center bg-no-repeat text-white">
             <div
-                className="absolute inset-0 bg-white/90"
+                className="absolute inset-0 bg-black/25"
             ></div>
-
             <div className="relative h-screen mx-auto sm:max-w-7xl flex flex-col justify-center md:items-start items-center gap-4 sm:gap-6">
                 <p className="text-sm sm:text-lg font-bold">Welcome!</p>
                 <h1 className="text-5xl font-extrabold sm:text-7xl">
@@ -17,14 +16,14 @@ function Hero() {
                     Software teacher and developer
                 </p>
                 <SocialLinks />
-                {/* <div className="absolute inset-x-0 bottom-16 flex items-center justify-center">
+                <div className="absolute inset-x-0 bottom-16 flex items-center justify-center">
                     <a
                         className="block w-auto rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500"
                         href="#"
                     >
                         Learn More
                     </a>
-                </div> */}
+                </div>
             </div>
         </div>
     );
